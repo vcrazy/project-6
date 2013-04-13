d873815e5735a63b2a5410080a697d93058f7a3d
vcrazy@abv.bg <vcrazy@abv.bg>
2013-04-13 17:07:42 +0300

    full effect

--- application/views/home_page/home_page_view.php
+++ application/views/home_page/home_page_view.php
@@ -1,1 +1,11 @@
-here we go!
+<div style="position: fixed; top: 0px; left: 0px; width: 1366px; height: 768px; overflow: hidden; display: none;" id="full_effect">
+	<div id="main" style="position: absolute; top: 0px; left: 0px; width: 1366px; height: 768px;">
+    	<img id="bgr" src="/img/BRG-top.png" width="1366" height="768" style="position: absolute; top: 0px; left: 0px; opacity: 1;" />
+        <h1 id="meesssenger" style="position: absolute; top: 110px; left: 490px; font-size: 110px; font-family: 'Segoe UI'; color: #fff; font-weight: 300; opacity: 0;">messenger</h1>
+    	<img id="magenta" src="/img/magentaTR.png" width="216" height="152" style="position: absolute; top: -152px; left: 402px;" />
+        <img id="cyan" src="/img/cyanTR.png" width="152" height="222" style="position: absolute; top: 376px; left: 1518px;" />
+        <img id="yellow" src="/img/yellowTR.png" width="221" height="152" style="position: absolute; top: 990px; left: 285px;" />
+        <img id="green" src="/img/greenTR.png" width="278" height="320" style="position: absolute; top: 166px; left: -278px;" />
+        <img id="bgr-transition" src="/img/BGR-transition.png" width="100%" height="100%" style="position: absolute; top: 0px; left: 0px; display: none;" />
+    </div>
+</div>
--- www/js/scripts.js
+++ www/js/scripts.js
@@ -39,18 +39,49 @@ $(document).ready(function(){
 		$('.modal').hide();
 	});
 
-        $('body').on('click', '.button_msg', function () {
-            var fromperson = $(this).data('sender');
-            var tocperson = $(this).data('receiver');
-            
-            var mymess = $(this).data('message');
-            var mymessid = $(this).data('message_id');
-            
-            $('#m_sender').html(fromperson);
-            $('#m_receiver').html(tocperson);
-            $('#m_message').html(mymess);
-            $('#m_sender_name').val(fromperson);
-            $('#addBookDialog').modal('show');
-        });
-        
+	$('body').on('click', '.button_msg', function () {
+		var fromperson = $(this).data('sender');
+		var tocperson = $(this).data('receiver');
+
+		var mymess = $(this).data('message');
+		var mymessid = $(this).data('message_id');
+
+		$('#m_sender').html(fromperson);
+		$('#m_receiver').html(tocperson);
+		$('#m_message').html(mymess);
+		$('#m_sender_name').val(fromperson);
+		$('#addBookDialog').modal('show');
+	});
+
+	function enterWebPage($time){
+		setTimeout(function(){
+
+			$("#bgr-transition").show();
+			$("#magenta").hide();
+			$("#cyan").hide();
+			$("#yellow").hide();
+			$("#green").hide();
+			$("#bgr").hide();
+			$("#main").animate({width: '40980px', height: '23040px', top: '-1650%', left: '-930%' }, 1500);
+		}, $time);
+	}
+
+	function revealLogo(){
+		$('#full_effect').show();
+		$("#magenta").animate({top: "265px", left: "402px"}, 700);
+		$("#cyan").animate({top: "376px", left: "466px"}, 700);
+		$("#yellow").animate({top: "446px", left: "285px"}, 700);
+		$("#green").animate({top: "166px", left: "284px"}, 700, function() {
+			$("#meesssenger").animate({top: "110px", left: "590px", opacity: 1}, 1200, function(){ enterWebPage(300); });
+		});
+
+		setTimeout(function(){
+			$('#full_effect').remove();
+		}, 3700);
+	}
+
+	if(typeof $.cookie('logo_shown6') == 'undefined'){
+		$.cookie('logo_shown6', 1);
+		revealLogo();
+	}
 });
