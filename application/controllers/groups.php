06afd0cd1ba17db7fe75eaddefd882d8d58881ac
vcrazy@abv.bg <vcrazy@abv.bg>
2013-04-13 19:22:04 +0300

    groups 2nd version

--- application/controllers/groups.php
+++ application/controllers/groups.php
@@ -2,8 +2,17 @@
 
 class Groups extends MY_Controller
 {
-	public function group()
+	public function __construct()
 	{
-		echo 'test';
+		parent::__construct();
+
+		$this->load->model('Model_groups');
+	}
+
+	public function group($group_id)
+	{
+		$group = $this->Model_groups->get($group_id);
+
+		
 	}
 }
