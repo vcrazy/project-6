69dbdd5f88a1c16f9b71452963b782fb749df11c
vcrazy@abv.bg <vcrazy@abv.bg>
2013-04-14 01:53:39 +0300

    change io port

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -2,9 +2,9 @@
 		<br />
 		<?php include('benchmark.php'); ?>
 		<?php if($this->session->userdata('is_logged')): ?>
-			<script src="http://project-6.loc:8742/socket.io/socket.io.js" type="text/javascript"></script>
+			<script src="http://141.0.170.38:8742/socket.io/socket.io.js" type="text/javascript"></script>
 			<script type="text/javascript">
-				var socket = io.connect('http://project-6.loc:8742');
+				var socket = io.connect('http://141.0.170.38:8742');
 				socket.emit('me', {user_id: <?php $user = $this->session->userdata('user'); echo $user['student_id']; ?>});
 
 				var sent_message = <?php echo isset($sent_message) && $sent_message ? 1 : 0; ?>,
