7b6b8c553b7af5adcb7216c7d98e1e8051c34c7d
vcrazy@abv.bg <vcrazy@abv.bg>
2013-04-13 13:21:59 +0300

    test

--- application/views/messages/unread_messages.php
+++ application/views/messages/unread_messages.php
@@ -13,22 +13,27 @@
         <?php foreach($all_unread_messages as $message): ?>
         <tr>
             <?php if ( isset($message['student_names']) ): ?>
+
             <td><?php echo $message['student_names'];?></td>
             <td>Теб</td>
             <td><?php echo mb_substr($message['message_text'], 0,40); if (strlen($message['message_text'])>40) echo '...';?></td>
             <td><?php echo $message['message_date'];?></td>
+
             <?php elseif ( isset($message['specialty_name']) ): ?>
+
             <td><?php echo $message['specialty_name'];?></td>
             <td>Теб</td>
             <td><?php echo mb_substr($message['message_text'], 0,40); if (strlen($message['message_text'])>40) echo '...';?></td>
             <td><?php echo $message['message_date'];?></td>
+
             <?php else: ?>
+
             <td><?php echo $message['group_subject'];?></td>
             <td>Теб</td>
             <td><?php echo mb_substr($message['message_text'], 0,40); if (strlen($message['message_text'])>40) echo '...';?></td>
             <td><?php echo $message['message_date'];?></td>
             <?php endif; ?>
-            <td><?php if (!empty($message['file_path'])) {echo '<a href="#"><img src="/img/appbar.disk.download.png" alt="Има прикачен файл"/></a>';}?></td>
+            <td><?php var_dump($message['file_path']); if (!empty($message['file_path'])) {echo '<a href="#"><img src="/img/appbar.disk.download.png" alt="Има прикачен файл"/></a>';} else echo '-';?></td>
         </tr>
         <?php endforeach; ?>
     </tbody>
