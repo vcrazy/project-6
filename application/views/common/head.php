c5b051b93f3c466ddd33c521330d8e34cde12017
vcrazy@abv.bg <vcrazy@abv.bg>
2013-04-13 09:34:03 +0300

    small fixes, added header

--- application/views/common/head.php
+++ application/views/common/head.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8" />
-		<title><?php echo $title; ?> | <?php echo ucfirst(SITE_NAME); ?></title>
+		<title>Project 6</title>
 		<link href="/css/styles.css?<?php echo $no_cache; ?>" rel="stylesheet" type="text/css" />
 		<script src="/js/jquery.min.js" type="text/javascript"></script>
 		<script src="/js/scripts.js?<?php echo $no_cache; ?>" type="text/javascript"></script>
--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,1 +1,2 @@
+<?php include('head.php'); ?>
 header
