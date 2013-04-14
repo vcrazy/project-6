dc57b13d47cf59e509aa0e7b7a6acda4e290bcf9
clanrbr <clan_rbr@yahoo.com>
2013-04-14 12:21:13 +0300

    header

--- application/views/common/header.php
+++ application/views/common/header.php
@@ -1,5 +1,3 @@
 <?php include('head.php'); ?>
-<div class="nav nav-pills navbar-static-top pull-right our_header">aaa</div>
-<div class="our_header page-header remove_margin">
-    <h1 class="remove_margin">some-header</h1>
+<div class="our_header navbar-static-top page-header remove_margin">
 </div>
